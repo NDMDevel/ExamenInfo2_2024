9de02809230134208133ce981d92d278116dd27e
NDMDevel <damian.n.marasco@gmail.com>
2024-05-23 20:31:30 -0300

    documentation added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main()
+
+/**
+@brief Esta funcion se despide del mundo
+
+Aca va todo el detalle. Que hace la funcion, porque, 
+cambios relevantes. ETC
+
+@param argc cuenta la cantidad de argumentos
+@param argv array of pointers to arguments
+
+@returns int indicating the status of the function
+*/
+int main(int argc, char *argv[])
 {
     printf("Good bye world!!\n");
     return 0;
